8c04e576ab04db795465c909157e3bf222ad0ce3
doctorseus <doctorseus@devlabs.pro>
2016-12-15 22:07:25 +0000

    QPU Makefile and simple executable skeleton.

---
+++ qpu/simple.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include <sys/time.h>
+
+#include "mailbox.h"
+#include "qpu.h"
+
+int main(int argc, char **argv) {
+    
+    return EXIT_SUCCESS;
+}
